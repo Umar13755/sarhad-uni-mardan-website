a0f7d5868d51c8ae2a97f323f85e6f4f08c31c89
Umar13755 <umarkhayyam570@gmail.com>
2024-12-12 19:11:42 +0500

    eslint off again

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const nextConfig = {
     eslint: {
       ignoreDuringBuilds: true,
     },
   };
-
-export default nextConfig;
+  
+  module.exports = nextConfig;
+  
